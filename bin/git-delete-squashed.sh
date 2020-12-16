1795c722408c7863b1a3e571554bd9205f522346
Junya Tokumori <j-tokumori@bns-g.com>
2020-12-16 12:47:08 +0900

    Github squash merged branch delete script

---
+++ bin/git-delete-squashed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+git checkout -q $current_branch && \
+  git for-each-ref refs/heads/ "--format=%(refname:short)" | \
+  while read branch; do
+    mergebase=$(git merge-base $current_branch $branch) && \
+      [[ $(git cherry $current_branch $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergebase -m _)) == "-"* ]] && \
+      git branch -d $branch;
+done
